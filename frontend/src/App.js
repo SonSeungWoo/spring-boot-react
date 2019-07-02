9acb69a2dcd9a4f58d7b3dc1dd34653b7b27be44
sonseungwoo <skyghy3342@naver.com>
2019-07-02 17:09:17 +0900

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+    state = {};
+
+    componentDidMount() {
+        console.log('111')
+        // setInterval(setIntervalthis.hello, 250);
+    }
+
+
+
+    hello = () => {
+    console.log('111')
+    fetch('/hello')
+.then(response => response.text())
+.then(message => {
+    console.log('message', message)
+    this.setState({message: message});
+});
+};
+
+render() {
+    return (
+        <div className="App">
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title">{this.state.message}</h1>
+        </header>
+        <button className="App-intro" onClick={this.hello}>
+        <h1>클릭하세요.</h1>
+        </button>
+        </div>
+);
+}
+}
+
+export default App;
